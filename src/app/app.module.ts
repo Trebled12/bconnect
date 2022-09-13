2253e394927372cff1b5616ee26731d4ab01ab20
ddensu <delbert.densu@hva.nl>
2022-09-13 12:26:59 +0200

    header & overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { OverviewComponent } from './components/overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule
